98c66638e632055b02605b424e52b4ee3025f39c
subra9minion <65240680+subra9minion@users.noreply.github.com>
2021-01-26 20:17:30 +0400

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -36,7 +36,7 @@ def load_data(filename):
     Load shopping data from a CSV file `filename` and convert into a list of
     evidence lists and a list of labels. Return a tuple (evidence, labels).
 
-    evidence should be a list of lists, where each list contains the
+    evidence is a list of lists, where each list contains the
     following values, in order:
         - Administrative, an integer
         - Administrative_Duration, a floating point number
@@ -56,7 +56,7 @@ def load_data(filename):
         - VisitorType, an integer 0 (not returning) or 1 (returning)
         - Weekend, an integer 0 (if false) or 1 (if true)
 
-    labels should be the corresponding list of labels, where each label
+    labels is the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
     evidence = []
@@ -120,13 +120,13 @@ def evaluate(labels, predictions):
     Given a list of actual labels and a list of predicted labels,
     return a tuple (sensitivity, specificty).
 
-    Assume each label is either a 1 (positive) or 0 (negative).
+    Assumes each label is either a 1 (positive) or 0 (negative).
 
-    `sensitivity` should be a floating-point value from 0 to 1
+    `sensitivity` is a floating-point value from 0 to 1
     representing the "true positive rate": the proportion of
     actual positive labels that were accurately identified.
 
-    `specificity` should be a floating-point value from 0 to 1
+    `specificity` is a floating-point value from 0 to 1
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
